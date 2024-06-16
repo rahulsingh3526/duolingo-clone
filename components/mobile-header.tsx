6ee102536d576e89a9dd5832d64b5b69cb903cf7
Rahul_Singh <koderrahul@gmail.com>
2024-06-16 15:40:31 +0530

    feat: siderbar component

--- components/mobile-header.tsx
+++ components/mobile-header.tsx
@@ -1,12 +1,13 @@
 import React from "react";
+import { MobileSidebar } from "./mobile-sidebar";
 
 export const MobileHeader = () => {
   return (
     <nav
       className="lg:hidden px-6 h-[50px] flex items-center 
-  bg-yellow-500 border-b fixed top-0 w-full z-50]"
+  bg-green-500 border-b fixed top-0 w-full z-50"
     >
-      MobileHeader
+      <MobileSidebar />
     </nav>
   );
 };
--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -1,5 +1,10 @@
 import React from "react";
 import { cn } from "@/lib/utils";
+import Image from "next/image";
+import Link from "next/link";
+import { SidebarItem } from "./sidebar-item";
+import { ClerkLoaded, ClerkLoading, UserButton } from "@clerk/nextjs";
+import { Loader } from "lucide-react";
 
 type Props = {
   style?: string;
@@ -9,11 +14,36 @@ export const Sidebar = ({ style }: Props) => {
   return (
     <div
       className={cn(
-        "flex bg-blue-500 h-full lg:w-[256px] lg:fixed left-0 top-0 px-4 border-r-2 flex-col",
+        "flex h-full lg:w-[256px] lg:fixed left-0 top-0 px-4 border-r-2 flex-col",
         style
       )}
     >
-      Sidebar
+      <Link href="/learn">
+        <div className="pt-8 pl-4 pb-7 flex items-center gap-x-3">
+          <Image src="mascot.svg" alt="Mascot" height={40} width={40} />
+          <h1 className="text-2xl font-extrabold text-green-600 tracking-wide">
+            Lingo
+          </h1>
+        </div>
+      </Link>
+      <div className="flex flex-col gap-y-2 flex-1">
+        <SidebarItem label="Learn" iconSrc="/learn.svg" href="/learn" />
+        <SidebarItem
+          label="Leaderboard"
+          iconSrc="/leaderboard.svg"
+          href="/leaderboard"
+        />
+        <SidebarItem label="Quests" iconSrc="/quests.svg" href="/quests" />
+        <SidebarItem label="Shop" iconSrc="/shop.svg" href="/shop" />
+      </div>
+      <div className="p-4">
+        <ClerkLoading>
+          <Loader className="h-5 w-5 text-muted-foreground animate-spin" />
+        </ClerkLoading>
+        <ClerkLoaded>
+          <UserButton afterSignOutUrl="/" />
+        </ClerkLoaded>
+      </div>
     </div>
   );
 };
